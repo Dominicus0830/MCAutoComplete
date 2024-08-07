734622a717222819b49fef2981b24408868321cd
kenter7317 <kenter7317@gmail.com>
2024-08-07 09:44:49 +0900

    init

--- src/main/java/per/dominicus0830/Main.java
+++ src/main/java/per/dominicus0830/Main.java
@@ -1,4 +1,4 @@
-package per.kenter7317;
+package per.dominicus0830;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
